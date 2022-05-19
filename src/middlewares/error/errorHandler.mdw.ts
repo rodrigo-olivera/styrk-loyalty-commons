08c40ddb32b6d6a1648e171abaddc46848395402
RoO <rodrigo.olivera@vtex.com.br>
2022-05-18 20:55:16 -0500

    errors

--- src/middlewares/error/errorHandler.mdw.ts
+++ src/middlewares/error/errorHandler.mdw.ts
@@ -3,9 +3,12 @@ import { NextFunction, Request, Response } from "express";
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     try {
         const { message, status } = JSON.parse(err.message)
+        console.log('error1', message, status);
+
         res.status(status || 500);
         res.send(message);
     } catch (error) {
+        console.log('error 2', error);
         res.status(500);
         res.send(err.message);
     }
