7a707a04e250d04729036cbd360697bc0699676c
RoO <rodrigo.olivera@vtex.com.br>
2022-05-18 20:15:22 -0500

    constants

--- src/constants/routes.msg.ts
+++ src/constants/routes.msg.ts
@@ -1,5 +1,5 @@
 export const ACCOUNTS = 'accounts';
-export const LOYALTY_CARDS = 'loyaltyCards';
+export const LOYALTY_CARDS = 'loyalty-cards';
 export const ACCOUNT = 'account';
 export const TRANSACTIONS = 'transactions';
 export const WORKSPACES = 'workspaces';
